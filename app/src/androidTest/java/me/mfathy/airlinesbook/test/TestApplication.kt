2fa4caa7f140002105917709bd88521dd567dceb
Mohammed Fathy <dev.mfathy@gmail.com>
2018-12-15 00:35:34 +0300

    feat: Adds a custom test runner to be used when integrating dagger with espresso for UI tests.

---
+++ app/src/androidTest/java/me/mfathy/airlinesbook/test/TestApplication.kt
@@ -0,0 +1,9 @@
+package me.mfathy.airlinesbook.test
+
+import android.app.Application
+
+/**
+ * Created by Mohammed Fathy on 15/12/2018.
+ * dev.mfathy@gmail.com
+ */
+class TestApplication : Application()
