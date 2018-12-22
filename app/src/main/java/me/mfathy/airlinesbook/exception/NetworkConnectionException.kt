9ade7328c6ef8ca5b3c2b27bd57d03184d496f3e
Mohammed Fathy <dev.mfathy@gmail.com>
2018-12-22 23:05:49 +0300

    feat: Added exception handling to show suitable messages to the user.

---
+++ app/src/main/java/me/mfathy/airlinesbook/exception/NetworkConnectionException.kt
@@ -0,0 +1,9 @@
+package me.mfathy.airlinesbook.exception
+
+/**
+ * Created by Mohammed Fathy on 21/12/2018.
+ * dev.mfathy@gmail.com
+ *
+ * A custom network connection exception.
+ */
+class NetworkConnectionException : Exception()
